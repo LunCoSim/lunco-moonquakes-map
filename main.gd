9f54edaa6abf0bc47da878b9ec260b787bb577c6
Difint <yarm-public@protonmail.com>
2022-10-01 18:43:38 +0700

    - Basic visualisation

--- main.gd
+++ main.gd
@@ -6,14 +6,14 @@ extends Node
 # var b = "text"
 
 #https://nssdc.gsfc.nasa.gov/planetary/lunar/lunar_sites.html
-var apollos = [
-				Vector2(0.67416, 23.47314), 
-				Vector2(-3.02, -23.42), 
-				Vector2(-3.64589, -17.47194),
-				Vector2(26.13239	, 3.63330), 
-				Vector2(-8.9734, 15.5011),
-				Vector2(20.1911, 30.7723)
-			]
+var apollos = {
+				"Apollo 11": Vector2(0.67416, 23.47314), 
+				"Apollo 12": Vector2(-3.02, -23.42), 
+				"Apollo 14": Vector2(-3.64589, -17.47194),
+				"Apollo 15": Vector2(26.13239	, 3.63330), 
+				"Apollo 16": Vector2(-8.9734, 15.5011),
+				"Apollo 17": Vector2(20.1911, 30.7723)
+			}
 #Load the resourse using preload
 const Mark = preload("res://models/mark.tscn")
 
@@ -33,7 +33,14 @@ enum EventType {
 	ARTIFICIAL
 }
 
-func get_event_type(event_type):
+var start_time = DateTime.new()
+var current_time = start_time
+var end_time = DateTime.new()
+
+var speed = 24 #hours/sec
+var speed_factor = 1
+
+func get_event_type(event_type: String):
 	#based on https://pds-geosciences.wustl.edu/lunar/urn-nasa-pds-apollo_seismic_event_catalog/data/gagnepian_2006_catalog.xml
 	if event_type == "M":
 		return EventType.NATURAL_IMPACT
@@ -44,6 +51,18 @@ func get_event_type(event_type):
 	else:
 		return EventType.ARTIFICIAL
 
+func to_datetime(datetime_str: String):
+	var dt = {
+		"year": int("19" + datetime_str.substr(0, 2)),
+		"month": int(datetime_str.substr(2, 2)),
+		"day": int(datetime_str.substr(4, 2)),
+		"hour": int(datetime_str.substr(6, 2)),
+		"minute": int(datetime_str.substr(8, 2))
+	}
+	
+	return DateTime.datetime(dt)
+
+	
 var colors = {
 	EventType.NATURAL_IMPACT: Color(0.5, 0.5, 0.5, 1),
 	EventType.DEEP_MOONQAUKE: Color(1, 0.5, 0.5, 1),
@@ -51,8 +70,9 @@ var colors = {
 	EventType.ARTIFICIAL: Color(0.5, 0.5, 1, 1)
 }
 
+
 func _ready():
-#	add_apollo_locations()
+	add_apollo_locations()
 	
 	moonquakes = LCDataSet.new()
 #	moonquakes.load("res://assets/levent.1008weber.csv")
@@ -66,27 +86,27 @@ func _ready():
 		
 		var depth = float(moonquakes.data[3][row_idx])
 		
-		var date = int(moonquakes.data[4][row_idx])
-		
-		print(type)
+		var date = to_datetime(moonquakes.data[4][row_idx])
+	
 		var pin = Mark.instance()
 		#You could now make changes to the new instance if you wanted
 		
 		pin.translation = spherical_to_cartesian(1+depth/100, lat, lon)
 		pin.set_color(colors[type])
+		pin.set_text(date.to_string())
 		#Attach it to the tree
 		$Moon.add_child(pin)
 	
 func add_apollo_locations():
 	var r = 1
 	
-	for loc in apollos:
+	for key in apollos:
 		var pin = Mark.instance()
 #		pin.pin.material.albedo = RGB()
 		#You could now make changes to the new instance if you wanted
-		
+		var loc = apollos[key]
 		pin.translation = spherical_to_cartesian(r, loc.x, loc.y)
-		
+		pin.set_text(key)
 		#Attach it to the tree
 		$Moon.add_child(pin)
 
@@ -104,9 +124,7 @@ static func spherical_to_cartesian(r, phi_degrees, theta_degrees):
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 
-var speed = 24 #hours/sec
 
-var current_time = 0
 
 #func _process(delta):
 #	pass
--- models/mark.gd
+++ models/mark.gd
@@ -19,4 +19,12 @@ func set_color(color=Color(0.92, 1, 1, 1.0)):
 	print("set_color")
 	var newMaterial = SpatialMaterial.new() #Make a new Spatial Material
 	newMaterial.albedo_color = color #Set color of new material
+	newMaterial.emission_enabled = true
+	newMaterial.emission = Color(color) #Set color of new material
+	newMaterial.emission_energy = 1 #Set color of new material
+	
+	
 	$pin.material = newMaterial #Assign new material to material overrride
+
+func set_text(text: String):
+	$site.text = text
