11d572dc4d9acb950ea8c59c031dadf1777daec1
Difint <yarm-public@protonmail.com>
2022-10-01 22:26:53 +0700

    - Added help

--- html/index.service.worker.js
+++ html/index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1664630844|20511744596";
+const CACHE_VERSION = "1664637982|26802282945";
 const CACHE_PREFIX = "LunCo: Moonquake-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
